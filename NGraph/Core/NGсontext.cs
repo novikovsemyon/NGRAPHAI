28e8d0561ff0336fee8b9e6c046c6182e43e534b
novikovsemyon <novikov.semyon@gmail.com>
2026-09-21 09:54:39 +0300

    Fix parameter helper preprocessor cleanup

--- NGraph/Core/NGсontext.cs
+++ NGraph/Core/NGсontext.cs
@@ -1,37 +1,50 @@
-﻿using System.Diagnostics.Contracts;
+using System.Diagnostics.Contracts;
 
 namespace NGraph.Core;
 
 public static class NgContext
-{/// <summary>
-    ///     Find a parameter in the instance or symbol by identifier
+{
+    /// <summary>
+    /// Find a parameter in the instance or element type by name.
     /// </summary>
-    /// <param name="element">The element</param>
-    /// <param name="parameter">The name of the parameter to be found</param>
     [JetBrains.Annotations.Pure]
     public static Parameter? _FindParameter(Element element, string parameter)
     {
         var instanceParameter = element.LookupParameter(parameter);
-        if (instanceParameter is not null) return instanceParameter;
+        if (instanceParameter is not null)
+        {
+            return instanceParameter;
+        }
 
         var elementTypeId = element.GetTypeId();
-        if (elementTypeId == ElementId.InvalidElementId) return null;
+        if (elementTypeId == ElementId.InvalidElementId)
+        {
+            return null;
+        }
 
         var elementType = element.Document.GetElement(elementTypeId);
         return elementType?.LookupParameter(parameter);
     }
+
+    /// <summary>
+    /// Find a parameter in the instance or element type by built-in identifier.
+    /// </summary>
+    [JetBrains.Annotations.Pure]
     public static Parameter? _FindParameter(Element element, BuiltInParameter parameter)
     {
         var instanceParameter = element.get_Parameter(parameter);
-        if (instanceParameter is not null) return instanceParameter;
+        if (instanceParameter is not null)
+        {
+            return instanceParameter;
+        }
 
         var elementTypeId = element.GetTypeId();
-        if (elementTypeId == ElementId.InvalidElementId) return null;
+        if (elementTypeId == ElementId.InvalidElementId)
+        {
+            return null;
+        }
 
         var elementType = element.Document.GetElement(elementTypeId);
         return elementType?.get_Parameter(parameter);
     }
-    #endif
-            
-    
 }
